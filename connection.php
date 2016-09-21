ddfe309b0e7a3541f6761eea34e080ad87c452e7
Andy <andytruong94@gmail.com>
2016-09-21 21:38:28 +0200

    Unique username registration

--- connection.php
+++ connection.php
@@ -1,25 +1,26 @@
 <?php
-
 class Database {
-private $connection;
 
+  public function signup($user, $home_a, $salt, $pass){
+    $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
+    $stmt = $connection->prepare("INSERT INTO Users (username, homeaddress,salt,password) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param('ssss', $user, $home_a, $salt, $pass);
+    $stmt -> execute();
+  }
 
+  public function getallProducts(){
+    $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
+    $stmt = $connection->prepare("SELECT * FROM products");
+    $stmt->execute();
+    return $stmt->get_result();
+  }
 
-public function signup($user,$home_a,$salt,$pass){
-  $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-  $stmt = $connection->prepare("INSERT INTO Users (username, homeaddress,salt,password) VALUES (?, ?, ?, ?)");
-  $stmt->bind_param('ssss', $user, $home_a, $salt, $pass);
-  $stmt -> execute();
+  public function hasUser($user) {
+    $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
+    $stmt = $connection->prepare("SELECT * FROM Users WHERE username=?");
+    $stmt->bind_param('s', $user);
+    $stmt->execute();
+    return mysqli_num_rows($stmt->get_result()) > 0;
+  }
 }
-
-public function getallProducts(){
-  $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-  $stmt = $connection->prepare("SELECT * FROM products");
-  $stmt->execute();
-  return $stmt->get_result();
-
-}
-
-}
-
  ?>
--- signup.php
+++ signup.php
@@ -1,8 +1,8 @@
 <?php
-require("includes/connection.php");
-require("includes/navigationbar.php");
+  require("includes/connection.php");
+  require("includes/navigationbar.php");
+?>
 
- ?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -16,40 +16,40 @@ require("includes/navigationbar.php");
              <form method="POST" action= "signup.php">
                 <td>UserName</td><td> <input type="text" name="user" required ></td>
               </tr>
-               <tr>
-                 <td>Home Address</td><td> <input type="text" name="homeaddress" required ></td>
-               </tr>
-                <tr>
-                  <td>Password</td><td> <input type="password" name="pass" required ></td>
-                </tr>
-                <tr>
-                  <td>Confirm Password </td><td><input type="password" name="cpass" required ></td>
-                </tr>
-                 <tr>
-                   <td><input id="button" type="submit" name="submit" value="Sign-Up" required></td>
-                 </tr>
-               </form>
-              </table>
-             </fieldset>
-             <?php
-             if(isset($_POST['submit'])){
-               if($_POST['user']== "" || $_POST['homeaddress']== "" ||$_POST['pass']== "" ||$_POST['cpass']== ""){
-                 echo "error: Fill in empty fields";
-               } else if($_POST['pass']!= $_POST['cpass']) {
-                 echo "error: Password does not match";
-               } else{
-                 $salt = uniqid();
-                 $password = hash('sha256',$salt.$_POST['pass']);
-                 $db = new Database();
-                 $db->signup($_POST['user'],$_POST['homeaddress'],$salt,$password);
-               }
-             }
-
-
-
-
-             ?>
-
-            </div>
-           </body>
-            </html>
+              <tr>
+                <td>Home Address</td><td> <input type="text" name="homeaddress" required ></td>
+              </tr>
+              <tr>
+                <td>Password</td><td> <input type="password" name="pass" required ></td>
+              </tr>
+              <tr>
+                <td>Confirm Password </td><td><input type="password" name="cpass" required ></td>
+              </tr>
+              <tr>
+                <td><input id="button" type="submit" name="submit" value="Sign-Up" required></td>
+              </tr>
+          </form>
+        </table>
+      </fieldset>
+        <?php
+          if(isset($_POST['submit'])){
+            if($_POST['user']== "" || $_POST['homeaddress']== "" ||$_POST['pass']== "" ||$_POST['cpass']== ""){
+              echo "error: Fill in empty fields";
+            } else if($_POST['pass']!= $_POST['cpass']) {
+              echo "error: Password does not match";
+            } else {
+              $db = new Database();
+              if($db->hasUser($_POST['user'])) {
+                echo "Username already exists!";
+              } else {
+                $salt = uniqid();
+                $password = hash('sha256',$salt.$_POST['pass']);
+                $db->signup($_POST['user'],$_POST['homeaddress'],$salt,$password);
+                echo "Registration successful!";
+              }
+            }
+          }
+        ?>
+    </div>
+  </body>
+</html>
