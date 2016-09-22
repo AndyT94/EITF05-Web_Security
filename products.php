9a9c37a11f06cba2edd8f8a43eedd1b6168ecba5
Andy <andytruong94@gmail.com>
2016-09-22 21:37:00 +0200

    Cart and shop done

--- products.php
+++ products.php
@@ -1,71 +1,93 @@
 <?php
-
 require("includes/connection.php");
 require("includes/navigationbar.php");
-
- ?>
-
-
+?>
 <!DOCTYPE html>
-
 <html>
 <head>
-
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link rel="stylesheet" href="css/reset.css" />
     <link rel="stylesheet" href="css/style.css" />
-
-    <title>Shopping cart</title>
-
+    <title>Shop</title>
 </head>
-
 <body>
-
     <div id="container">
-
         <div id="main">
           <h1> Product List </h1>
           <table>
+            <form method="POST" action="products.php">
               <tr>
                   <th> Name </th>
                   <th> Description </th>
                   <th> Price </th>
               </tr>
-
               <?php
-
-               $db = new Database();
-               $result = $db->getallProducts();
-
-
-
-              while($row = $result -> fetch_assoc()) {
-
-
-               ?>
-
+                $db = new Database();
+                $result = $db->getallProducts();
+                while($row = $result -> fetch_assoc()) {
+              ?>
               <tr>
                 <td><?php echo $row["name"]?></td>
                 <td> <?php echo $row["description"]?></td>
                 <td> <?php echo $row["price"]?></td>
-                <td><a href="#"> Add to cart</a></td>
-
+                <td><button name="submit" name="submit" value= <?php echo $row['id_product']?>>Add to cart</td>
+                  <td><button name="remove" name="remove" value= <?php echo $row['id_product']?>>Remove from cart</td>
               </tr>
-
               <?php
-
                 }
-               ?>
-
+              ?>
+            </form>
           </table>
-
         </div><!--end main-->
 
+        <?php
+          if(isset($_POST['submit'])){
+            if(!isset($_SESSION['cart'])){
+              $_SESSION['cart'] = array();
+            }
+            if(!isset($_SESSION['cart'][$_POST['submit']])) {
+              $_SESSION['cart'][$_POST['submit']] = 1;
+            } else {
+              $_SESSION['cart'][$_POST['submit']]++;
+            }
+          }
+
+          if(isset($_POST['remove'])){
+            if(!isset($_SESSION['cart'][$_POST['remove']]) || $_SESSION['cart'][$_POST['remove']]<=1) {
+              unset($_SESSION['cart'][$_POST['remove']]);
+            } else {
+              $_SESSION['cart'][$_POST['remove']]--;
+            }
+          }
+        ?>
+
         <div id="sidebar">
+          <h1> Cart </h1>
+          <table>
+          <tr>
+            <th> Product name </th>
+            <th> Quantity </th>
+            <th> Price </th>
+          </tr>
 
+          <?php
+            $db = new Database();
+            foreach ($_SESSION['cart'] as $id => $quantity) {
+              $result = $db->getIDProduct($id);
+              while($row = $result -> fetch_assoc()) {
+          ?>
+                <tr>
+                  <td><?php echo $row["name"]?></td>
+                  <td> <?php echo $quantity?></td>
+                  <td> <?php echo $row["price"]*$quantity?></td>
+                </tr>
+            <?php
+              }
+            }
+          ?>
+          <td><a href="payment.php">Go to payment</a></td>
+        </table>
         </div><!--end sidebar-->
-
     </div><!--end container-->
-
 </body>
 </html>
