fa483a2c81e7ca4d01a99505c82e841c8df9170c
Andy <andytruong94@gmail.com>
2016-10-06 17:21:51 +0200

    Removed prepare stmts

--- NotSecure/includes/connection.php
+++ NotSecure/includes/connection.php
@@ -3,38 +3,41 @@ class Database {
 
   public function signup($user, $home_a, $salt, $pass){
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("INSERT INTO Users (username, homeaddress,salt,password) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param('ssss', $user, $home_a, $salt, $pass);
-    $stmt -> execute();
+    $connection->query("INSERT INTO Users (username, homeaddress,salt,password) VALUES ('$user', '$home_a', '$salt', '$pass')");
+//    $stmt = $connection->prepare("INSERT INTO Users (username, homeaddress,salt,password) VALUES (?, ?, ?, ?)");
+//    $stmt->bind_param('ssss', $user, $home_a, $salt, $pass);
+//    $stmt -> execute();
   }
 
   public function getallProducts(){
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("SELECT * FROM products");
-    $stmt->execute();
-    return $stmt->get_result();
+    $stmt = $connection->query("SELECT * FROM products");
+//    $stmt = $connection->prepare("SELECT * FROM products");
+//    $stmt->execute();
+    return $stmt;
   }
 
   public function hasUser($user) {
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("SELECT * FROM Users WHERE username=?");
-    $stmt->bind_param('s', $user);
-    $stmt->execute();
-    return mysqli_num_rows($stmt->get_result()) > 0;
+    $stmt = $connection->query("SELECT * FROM Users WHERE username='$user'");
+//    $stmt = $connection->prepare("SELECT * FROM Users WHERE username=?");
+//    $stmt->bind_param('s', $user);
+//    $stmt->execute();
+    return mysqli_num_rows($stmt) > 0;
   }
 
   public function isAuthenticated($user, $pass) {
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("SELECT salt FROM Users WHERE username=?");
-    $stmt->bind_param('s', $user);
-    $stmt->execute();
-    $res = $stmt->get_result();
+    $res = $connection->query("SELECT salt FROM Users WHERE username COLLATE latin1_general_cs ='$user'");
+  //  $stmt->bind_param('s', $user);
+  //  $stmt->execute();
+//    $res = $stmt->get_result();
     if(mysqli_num_rows($res) > 0) {
       $hashedPass = hash('sha256',$res->fetch_row()[0].$pass);
-      $stmt = $connection->prepare("SELECT * FROM Users WHERE username=? AND password=?");
-      $stmt->bind_param('ss', $user, $hashedPass);
-      $stmt->execute();
-      return mysqli_num_rows($stmt->get_result()) > 0;
+      $stmt = $connection->query("SELECT * FROM Users WHERE username='$user' AND password='$hashedPass'");
+//      $stmt->bind_param('ss', $user, $hashedPass);
+  //    $stmt->execute();
+      return mysqli_num_rows($stmt) > 0;
     } else {
       return false;
     }
@@ -42,24 +45,27 @@ class Database {
 
   public function getIDProduct($id) {
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("SELECT * FROM products WHERE id_product=?");
-    $stmt->bind_param('i', $id);
-    $stmt->execute();
-    return $stmt->get_result();
+    $stmt = $connection->query("SELECT * FROM products WHERE id_product='$id'");
+//    $stmt = $connection->prepare("SELECT * FROM products WHERE id_product=?");
+//    $stmt->bind_param('i', $id);
+//    $stmt->execute();
+    return $stmt;
   }
 
   public function getallComments(){
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("SELECT * FROM comments");
-    $stmt->execute();
-    return $stmt->get_result();
+    $stmt = $connection->query("SELECT * FROM comments");
+//    $stmt = $connection->prepare("SELECT * FROM comments");
+//    $stmt->execute();
+    return $stmt;
   }
 
   public function postComment($text){
     $connection = mysqli_connect('localhost','Admin','password','Eitf05') or die ('Could not connect');
-    $stmt = $connection->prepare("INSERT INTO comments(text) VALUES(?)");
-    $stmt->bind_param('s',$text);
-    $stmt->execute();
+    $connection->query("INSERT INTO comments (text) VALUES ('$text')");
+    //$stmt = $connection->prepare("INSERT INTO comments(text) VALUES(?)");
+    //$stmt->bind_param('s',$text);
+    //$stmt->execute();
 
   }
 }
